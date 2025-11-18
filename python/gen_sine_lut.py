cd560817c06c7de8465308ec0c64519640f6194b
Daniel Santos <danielsantos7@outlook.com.br>
2025-11-17 22:36:01 -0300

    Fix formatting in Makefile and update variable names in sine lookup table generation script for clarity

--- python/gen_sine_lut.py
+++ python/gen_sine_lut.py
@@ -11,8 +11,8 @@ s = [m.sin(k*n) for n in range(0, 2**N)]
 MAX_VALUE = 2**R-1
 y = [int(x*MAX_VALUE) for x in s]
 
-print(f'type sine_lut is array (0 to {2**N-1}) of std_logic_vector({R-1} downto 0);')
-print('constant SINE_TABLE : sine_lut := (')
+print(f'type sine_lut_array is array (0 to {2**N-1}) of std_logic_vector({R-1} downto 0);')
+print(f'constant SINE_TABLE : sine_lut_array := (')
 for i, v in enumerate(y):
     sep = "," if i < 2**N - 1 else ""
     # Converte o valor inteiro para string hexadecimal
@@ -20,4 +20,4 @@ for i, v in enumerate(y):
     hex_digits = (R + 3) // 4
     hex_str = format(v, f'0{hex_digits}X')
     print(f'    X"{hex_str}"{sep}')
-print(');')
+print(f');')
